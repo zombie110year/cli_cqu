a1f5ec0b196bd72fdb1e08d28afc4dbe893ce71b
zombie110year <zombie110year@outlook.com>
2020-02-17 17:19:10 +0800

    重构（route）：一个 route 用一个方法

--- cli_cqu/data/route.py
+++ cli_cqu/data/route.py
@@ -30,54 +30,38 @@ class Parsed:
     class TeachingArragement:
         "教学安排模块"
 
-        # todo: 只使用 get 方法
         @staticmethod
-        def personal_cources(s: Session,
-                             method="get",
-                             data: dict = None) -> dict:
+        def personal_cources(s: Session) -> dict:
             "解析个人课表页面，获取可得的信息"
-            method = method.lower()
             url = f"{HOST.PREFIX}{Route.TeachingArragement.personal_cources}"
-            if method == "get":
-                # 需要填写的表单数据以及说明
-                resp = s.request(method, url)
-                html = BeautifulSoup(resp.text, "lxml")
-                el_学年学期 = html.select("select[name=Sel_XNXQ] > option")
-                学年学期 = [{
-                    "text": i.text,
-                    "value": int(i.attrs["value"])
-                } for i in el_学年学期]
-                el_是否选择周次 = html.select_one("input[name=zc_flag]")
-                el_设定周次 = html.select("input[name=zc_input]")
-                是否周次 = {"text": "周次", "value": el_是否选择周次.attrs["value"]}
-                周次 = [{
-                    "text": i.text,
-                    "value": i.attrs["value"]
-                } for i in el_设定周次]
-                el_排序 = html.select("select[name=px] > option")
-                排序 = [{
-                    "text": i.text,
-                    "value": int(i.attrs["value"])
-                } for i in el_排序]
-
-                return {
-                    "Sel_XNXQ": 学年学期,
-                    "zc_flag": 是否周次,
-                    "zc_input": 周次,
-                    "px": 排序,
-                    "rad": {
-                        "text": "总是 on，不知道干嘛的",
-                        "value": "on"
-                    },
-                    "###": "当 zc_flag 为 0 时，可以不传入 zc_* 两个参数"
-                }
-
-            elif method == "post":
-                # 提交表单，查询课表
-                logging.error("你应当将表单提交到 /znpk/Pri_StuSel_rpt.aspx")
-                return {"error": "should post to /znpk/Pri_StuSel_rpt.aspx"}
-            else:
-                return {"raw": s.request(method, url)}
+            # 需要填写的表单数据以及说明
+            resp = s.get(url)
+            html = BeautifulSoup(resp.text, "lxml")
+            el_学年学期 = html.select("select[name=Sel_XNXQ] > option")
+            学年学期 = [{
+                "text": i.text,
+                "value": int(i.attrs["value"])
+            } for i in el_学年学期]
+            el_是否选择周次 = html.select_one("input[name=zc_flag]")
+            el_设定周次 = html.select("input[name=zc_input]")
+            是否周次 = {"text": "周次", "value": el_是否选择周次.attrs["value"]}
+            周次 = [{"text": i.text, "value": i.attrs["value"]} for i in el_设定周次]
+            el_排序 = html.select("select[name=px] > option")
+            排序 = [{
+                "text": i.text,
+                "value": int(i.attrs["value"])
+            } for i in el_排序]
+            return {
+                "Sel_XNXQ": 学年学期,
+                "zc_flag": 是否周次,
+                "zc_input": 周次,
+                "px": 排序,
+                "rad": {
+                    "text": "总是 on，不知道干嘛的",
+                    "value": "on"
+                },
+                "###": "当 zc_flag 为 0 时，可以不传入 zc_* 两个参数"
+            }
 
         @staticmethod
         def personal_cources_table(
